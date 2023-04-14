2ecad016c800b8cc770cac8094cfc91608b36054
Logan Gunn <lgunn4@uwo.ca>
2023-04-13 21:59:37 -0400

    made it rails app instead scre you react

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+    before_action :set_header_details
+
+    def set_header_details
+        @categories = Category.all
+        @blog_configuration = ::Configuration.first
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
+  
+  root "home#index"
+  get 'posts/:id', to: 'posts#show', as: 'post'
 
   post "/graphql", to: "graphql#execute"
   get "rails/csrf-token", to: "rails#csrf_token"
