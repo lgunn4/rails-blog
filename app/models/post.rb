4d6e2220a9fbed6933efe825529405a77c5872d4
Logan Gunn <44077842+lgunn4@users.noreply.github.com>
2024-03-12 14:59:32 -0400

    🔄 Ordering

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,12 +9,10 @@ class Post < ApplicationRecord
   before_save :generate_handle
 
   def self.posts_by_year
-    where(published: true).each_with_object({}) do |post, posts_by_year|
+    where(published: true).order(created_at: :asc).each_with_object({}) do |post, posts_by_year|
       post_year = post.created_at.year
       posts_by_year[post_year] ||= []
       posts_by_year[post_year].append(post)
-    end.each do |year, posts|
-      posts.sort_by! { |post| post.created_at }
     end.sort_by { |key, _| key }.reverse.to_h
   end
 
